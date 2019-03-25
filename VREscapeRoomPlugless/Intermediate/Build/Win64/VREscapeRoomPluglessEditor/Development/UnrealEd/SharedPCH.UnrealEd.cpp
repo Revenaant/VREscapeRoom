dc9cf04924fff4c34ea4998ae741ec872f36c555
Rodrigo Sanchez <rodrigo_jsd@hotmail.com>
2019-03-25 11:30:42 +0100

    Put the plugin in the empty project

---
+++ VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
@@ -0,0 +1,1 @@
+#include "E:/UnrealProjects/VREscapeRoom/VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
---
+++ VREscapeRoomPlugless/Source/VREscapeRoomPlugless/ClassToBuildTheProject.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VRESCAPEROOMPLUGLESS_API ClassToBuildTheProject
+{
+public:
+	ClassToBuildTheProject();
+	~ClassToBuildTheProject();
+};
