cf9c30cec07250c8f9a5f4d92374b52995321137
Slavi Stoev <sl_stoev@abv.bg>
2019-03-25 16:58:34 +0100

    Built with VS. Kinda works!

--- VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/Engine/SharedPCH.Engine.cpp
+++ VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "E:/UnrealProjects/VREscapeRoom/VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/Slavi/Desktop/Oops/Repo/VREscapeRoom/VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/Engine/SharedPCH.Engine.h"
--- VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
+++ VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
@@ -1,1 +1,1 @@
-#include "E:/UnrealProjects/VREscapeRoom/VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
+#include "C:/Users/Slavi/Desktop/Oops/Repo/VREscapeRoom/VREscapeRoomPlugless/Intermediate/Build/Win64/VREscapeRoomPluglessEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
