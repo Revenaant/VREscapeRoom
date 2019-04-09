ba9c379b06283d9b06a83ea61ac7e2f102ac0ed2
Revenaant <rodrigo_jsd@hotmail.com>
2019-04-09 15:11:03 +0200

    Placing object in socket + trying to remake sliceable objects

--- Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 #include "C:/Users/Slaggy/Desktop/Dev/VR_ER/VREscapeRoom/Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.h"
+=======
+#include "C:/Users/rodri/UnrealProjects/VREscapeRoom/Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.h"
+>>>>>>> Stashed changes
--- Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
+++ Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 #include "C:/Users/Slaggy/Desktop/Dev/VR_ER/VREscapeRoom/Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
+=======
+#include "C:/Users/rodri/UnrealProjects/VREscapeRoom/Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
+>>>>>>> Stashed changes
