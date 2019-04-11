c5741bffa9fd147e85665340a4fadb4473db0030
Slavi Stoev <sl_stoev@abv.bg>
2019-04-11 12:40:37 +0200

    I cri. C++ no work.

--- Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.cpp
+++ Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 #include "C:/Users/Slaggy/Desktop/Dev/VR_ER/VREscapeRoom/Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.h"
-=======
-#include "C:/Users/rodri/UnrealProjects/VREscapeRoom/Submarine/Intermediate/Build/Win64/SubmarineEditor/Development/Engine/SharedPCH.Engine.h"
->>>>>>> Stashed changes
